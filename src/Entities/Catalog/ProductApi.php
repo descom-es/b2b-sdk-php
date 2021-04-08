9545723586f5f171a998d6123e5d9bcc515a35ae
Juan José Llorens <llorensjj@gmail.com>
2021-04-08 11:11:04 +0200

    Implement products

---
+++ src/Entities/Catalog/ProductApi.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Descom\B2b\Entities\Catalog;
+
+use Descom\B2b\Entities\Api;
+use Descom\B2b\Models\Catalog\BrandModel;
+use Descom\B2b\Models\Catalog\ProductModel;
+
+class ProductApi extends Api
+{
+    private string $uri = '/api/v1/products';
+
+    public function index(array $params = []): ?object
+    {
+        $response = $this->connection->call(
+            'GET',
+            $this->uri,
+            null,
+            $params,
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function show(int $id): ?object
+    {
+        $response = $this->connection->call(
+            'GET',
+            "$this->uri/$id",
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function store(ProductModel $model): ?object
+    {
+        $response = $this->connection->call(
+            'POST',
+            $this->uri,
+            $model->toArray()
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function update(int $id, ProductModel $model): ?object
+    {
+        $response = $this->connection->call(
+            'PUT',
+            "$this->uri/$id",
+            $model->toArray()
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function destroy(int $id): ?bool
+    {
+        return $this->connection->call(
+            'DELETE',
+            "$this->uri/$id",
+        )->successful;
+    }
+}
---
+++ src/Models/Catalog/ProductModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Descom\B2b\Models\Catalog;
+
+use Descom\B2b\Models\Model;
+
+class ProductModel extends Model
+{
+    public string $sku;
+    public float $price;
+    public string $gtin8;
+    public string $name;
+    public int $attribute_set_id;
+    public string $type;
+    public ?float $retail_price;
+    public bool $visibility;
+    public ?int $parent_id;
+    public array $attribute_options;
+}
