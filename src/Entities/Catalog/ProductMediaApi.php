3bbabbf4d6c8d981a5082a42c4c28b9f9d6c9efd
Juan José Llorens <llorensjj@gmail.com>
2021-04-08 13:01:28 +0200

    Implement Medias and ProductMedias

---
+++ src/Entities/Catalog/ProductMediaApi.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Descom\B2b\Entities\Catalog;
+
+use Descom\B2b\Entities\Api;
+
+class ProductMediaApi extends Api
+{
+    private string $uri = '/api/v1/products';
+
+    public function index(int $idProduct, array $params = []): ?array
+    {
+        $response = $this->connection->call(
+            'GET',
+            "$this->uri/$idProduct/medias",
+            null,
+            $params,
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function attach(int $idProduct, int $idMedia): ?array
+    {
+        $response = $this->connection->call(
+            'PUT',
+            "$this->uri/$idProduct/medias/$idMedia"
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function detach(int $idProduct, int $idMedia): ?array
+    {
+        $response = $this->connection->call(
+            'DELETE',
+            "$this->uri/$idProduct/medias/$idMedia"
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function cover(int $idProduct, int $idMedia): ?array
+    {
+        $response = $this->connection->call(
+            'PUT',
+            "$this->uri/$idProduct/medias/$idMedia/cover"
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+}
