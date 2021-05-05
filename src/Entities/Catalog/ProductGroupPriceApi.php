7cf0346285ef95e4b1b3d51befae50d7bdce1902
Cesar <cesargb@gmail.com>
2021-05-05 13:46:03 +0200

    add api product.group price

---
+++ src/Entities/Catalog/ProductGroupPriceApi.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Descom\B2b\Entities\Catalog;
+
+use Descom\B2b\Entities\Api;
+use Descom\B2b\Models\Catalog\ProductGroupPriceModel;
+
+class ProductGroupPriceApi extends Api
+{
+    private string $uri = '/api/v1/products';
+
+
+    public function attach(int $idProduct, int $idGroup, ProductGroupPriceModel $model): ?array
+    {
+        $response = $this->connection->call(
+            'PUT',
+            "{$this->uri}/{$idProduct}/groups/{$idGroup}",
+            $model->toArray()
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+
+    public function detach(int $idProduct, int $idGroup): int
+    {
+        $response = $this->connection->call(
+            'DELETE',
+            "{$this->uri}/{$idProduct}/groups/{$idGroup}",
+        );
+
+        if ($response->successful) {
+            return $response->data;
+        }
+
+        return null;
+    }
+}
